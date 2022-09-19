e2c831571d7766f9763226bb0ea74713fd445013
RohithMg <86477512+314182@users.noreply.github.com>
2022-09-19 11:44:14 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,13 @@ int main(){
   cout << "enter two integers";
   cin >> firstnum;
   cin >> secondnum;
-  if(firstnum >= 0 && secondnum >=0){
   sum = add(firstnum,secondnum);
   cout << "The sum of two num is:"<< sum << endl;
   sub = subt(firstnum,secondnum);
   cout << "The sub of two num is:"<< sub << endl;
   mul = mult(firstnum,secondnum);
   cout << "The mul of two num is:"<< mul << endl;
+  if(secondnum!=0){
   div = divi(firstnum,secondnum);
   cout << "The div of two num is:"<< div << endl;
   }
